77f785e81a1bbe0689892a04d8847749ab4fc762
張傑智 <iii@iii.org.tw>
2018-06-01 13:28:11 +0800

    顧問01版
    第一版

--- aicoco/settings.py
+++ aicoco/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'root',
     'portfolio',
     'MemberCentre',
+    'consultant',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ consultant/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+# class Consultant(models.Model):
+#     姓名 = models.CharField(max_length=20,null=False)
+#     學歷 = models.CharField(max_length=100,null=False)
+#     經歷 = models.CharField(max_length=100,null=False)
+#     證照 = models.CharField(max_length=100,null=False)
+    
+#     class Meta:
+#         db_table = "consultants"
+
+class Consultant(models.Model):
+    姓名 = models.CharField(max_length=20)
+    學歷 = models.CharField(max_length=100)
+    經歷 = models.CharField(max_length=100)
+    證照 = models.CharField(max_length=100)
+    image = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'consultants'
